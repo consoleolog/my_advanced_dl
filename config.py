a80fc64c98c1a4fb9fec54a22f79b926f0f2c37b
consolelog <leopportunity5@gmail.com>
2024-07-30 17:56:38 +0900

    [5] 회귀분석 테스트

--- config.py
+++ config.py
@@ -3,5 +3,17 @@ import os
 
 load_dotenv()
 
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
 BEDROCK_AWS_ACCESS_KEY = os.getenv('BEDROCK_AWS_ACCESS_KEY')
 BEDROCK_AWS_SECRET_KEY = os.getenv('BEDROCK_AWS_SECRET_KEY')
+
+NEO4J_URI = os.getenv('NEO4J_URI')
+NEO4J_USER = os.getenv('NEO4J_USER')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+
+
+
+
+
+
